fa4857715aa2086763dcbdb183766afd52e3de3e
hoangtd4 <hoangtd4@vng.com.vn>
2025-07-17 09:34:11 +0700

    feat: remove wrong abstraction address family

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use tun::{AbstractDevice, Configuration};
-use std::{env::consts::UTUN_ADDRESS_FAMILY, io::{self, Read}};
+use core::convert::TryInto;
+use std::{io::{self, Read}};
 
 fn main() -> io::Result<()> {
     let mut config = Configuration::default();
@@ -14,22 +15,17 @@ fn main() -> io::Result<()> {
     let mut tun_device = tun::create(&config)?;
 
     let tun_name = tun_device.tun_name();
-    println!("Đã tạo giao diện TUN thành công: {:?}", tun_name);
-    println!("Địa chỉ IP: 10.0.0.1/24");
+    println!("create TUN interface success: {:?}", tun_name);
+    println!("IP: 10.0.0.1/24");
     println!("Destination: 10.0.0.254");
 
-    // still need to read 1504 bytes here, have 4 bytes for address family
-    let mut MTU =  1500; // maximum transmission unit
-    let mut UTUN_ADDRESS_FAMILY = 4; // AF_INET (0x02): IPv4 or AF_INET6 (0x1E): IPv6
-    let mut CHUNK =  MTU + UTUN_ADDRESS_FAMILY;
-    let mut buf = [0u8; CHUNK];
+    const MTU: usize = 1500; // maximum transmission unit
+    let mut buf = [0u8; MTU];
     
     loop {
-        // bytes này bao gồm header và payload
-        // có thể lấy ra và biết được là type nào, IP, ICMP
         let nbytes = tun_device.read(&mut buf).unwrap();
 
-        println!("read {} bytes {:x?}", nbytes, &buf[..nbytes]);
+        println!("read {}, bytes {:x?}", nbytes, &buf[..nbytes]);
     }
     Ok(())
 }
